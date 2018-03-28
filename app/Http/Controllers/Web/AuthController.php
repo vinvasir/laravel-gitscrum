fa01cf7fd4ab7c2f12abf2920010b433b02a10e2
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-03-28 16:07:31 -0400

    redirect to sprints.index upon authentication

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -57,6 +57,6 @@ class AuthController extends Controller
 
         Auth::loginUsingId($user->id);
 
-        return redirect()->route('user.dashboard');
+        return redirect()->route('sprints.index');
     }
 }
